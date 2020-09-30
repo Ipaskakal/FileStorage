5e4533884221440cd60fd0dfd7813a7810cddc37
Maxym Domalchuk <ipaskakal123@gmail.com>
2020-09-30 22:56:16 +0300

    elasticsearch port and host config from properties in RestClientConfig.java

---
+++ src/main/java/com/testtask/filestorage/repository/RestClientConfig.java
@@ -0,0 +1,27 @@
+package com.testtask.filestorage.repository;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+@Configuration
+public class RestClientConfig extends AbstractElasticsearchConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        String hostAndPort= env.getProperty("elasticsearch.host") + ":"+ env.getProperty("elasticsearch.port");
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(hostAndPort)
+                .build();
+            return RestClients.create(clientConfiguration).rest();
+    }
+}
